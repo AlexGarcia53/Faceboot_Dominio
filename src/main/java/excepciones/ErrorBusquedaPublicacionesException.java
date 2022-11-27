1efdeaa34c0c7a1b095a00c19bf3487c2fd1f719
Juan-Sotelo <juan.sotelo229391@potros.itson.edu.mx>
2022-11-26 22:14:56 -0700

    near the village, the peacefull village the lion sleeps tonight

---
+++ src/main/java/excepciones/ErrorBusquedaPublicacionesException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author Admin
+ */
+public class ErrorBusquedaPublicacionesException extends RuntimeException{
+
+    public ErrorBusquedaPublicacionesException() {
+        super();
+    }
+
+    public ErrorBusquedaPublicacionesException(String message) {
+        super(message);
+    }
+    
+}
